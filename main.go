dfa4e05a6c5e32f547c7bc6b6b2b7278a789e303
louissaadgo <68654123+louissaadgo@users.noreply.github.com>
2020-08-15 02:13:12 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type number int
+
+func (m number) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	data := struct {
+		Method        string
+		URL           *url.URL
+		Submissions   map[string][]string
+		Header        http.Header
+		Host          string
+		ContentLength int64
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+		req.Header,
+		req.Host,
+		req.ContentLength,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var num number
+	http.ListenAndServe(":8080", num)
+}
